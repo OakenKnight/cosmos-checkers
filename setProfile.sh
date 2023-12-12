1c8cc8478895f7849a98df600d1bb25bfb8bb015
OakenKnight <aleksandarignjatijevic@informal.systems>
2023-12-12 09:13:23 +0100

    checkers game, without autoexpiring

---
+++ setProfile.sh
@@ -0,0 +1,3 @@
+export alice=$(checkersd keys show alice -a --keyring-backend test)
+export bob=$(checkersd keys show bob -a --keyring-backend test)
+
--- x/checkers/types/keys.go
+++ x/checkers/types/keys.go
@@ -1,5 +1,7 @@
 package types
 
+import "time"
+
 const (
 	// ModuleName defines the module name
 	ModuleName = "checkers"
@@ -20,3 +22,25 @@ const (
 func KeyPrefix(p string) []byte {
 	return []byte(p)
 }
+
+const (
+	SystemInfoKey = "SystemInfo-value-"
+
+	GameCreatedEventType      = "new-game-created" // Indicates what event type to listen to
+	GameCreatedEventCreator   = "creator"          // Subsidiary information
+	GameCreatedEventGameIndex = "game-index"       // What game is relevant
+	GameCreatedEventBlack     = "black"            // Is it relevant to me?
+	GameCreatedEventRed       = "red"              // Is it relevant to me?
+
+	MovePlayedEventType      = "move-played"
+	MovePlayedEventCreator   = "creator"
+	MovePlayedEventGameIndex = "game-index"
+	MovePlayedEventCapturedX = "captured-x"
+	MovePlayedEventCapturedY = "captured-y"
+	MovePlayedEventWinner    = "winner"
+
+	MovePlayedEventBoard = "board"
+	MaxTurnDuration      = time.Duration(24 * 3_600 * 1000_000_000) // 1 day
+	DeadlineLayout       = "2006-01-02 15:04:05.999999999 +0000 UTC"
+	NoFifoIndex          = "-1"
+)
